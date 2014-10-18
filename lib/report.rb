aa6ee73d1b1af2fd10a98eb11ae25c16b7e3fb6f
James McCarthy <james@thisishatch.co.uk>
2014-10-18 20:55:07 +0700

    Add a Report active model class.
    Signed-off-by: James McCarthy <james@thisishatch.co.uk>

---
+++ lib/report.rb
@@ -0,0 +1,26 @@
+class Report
+
+  include ActiveModel::Model
+
+  delegate :type, :title, :from, :to, :period, :month, :file_name, :stream, to: :report, allow_nil: true
+
+  def self.all
+    Reports.all.map { |klass| new('type' => String(klass)) }
+  end
+
+  def initialize(options = {})
+    type = String(options.delete('type'))
+
+    klass = Reports.all.find { |mod| String(mod).demodulize.underscore.gsub(/_report$/, '') == type }
+
+    @report = klass.new(options) if klass
+  end
+
+  def report
+    @report
+  end
+
+  def new_record?; true; end
+  def persisted?; false; end
+
+end
