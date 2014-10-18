ce86d21ceddda2b3d4aea159e054943607ca7cc2
James McCarthy <james@thisishatch.co.uk>
2014-10-18 20:51:51 +0700

    Add Loading of reports on initialisation.
    Signed-off-by: James McCarthy <james@thisishatch.co.uk>

--- lib/reports.rb
+++ lib/reports.rb
@@ -1,7 +1,27 @@
 require 'csv'
+require 'reports/row'
 require 'reports/base'
 require 'reports/scoped'
 require 'reports/periodic'
 require 'reports/periodic/scopes'
+require 'report'
 require 'reports/version'
 require 'reports/railtie' if defined?(Rails) && Rails::VERSION::MAJOR >= 3
+module Reports
+
+  def self.all
+    @all ||= Base.descendants.reject { |mod| ["Reports::Scoped", "Reports::Periodic"].include?(mod.to_s) }
+  end
+
+  def self.reload #:nodoc:
+    load_paths = [File.expand_path('app/reports', Rails.root)]
+
+    load_paths.each do |path|
+      Dir[File.join(path, '**', '*.rb')].sort.each do |file|
+        load file
+      end
+    end
+
+  end
+
+end
--- lib/reports/railtie.rb
+++ lib/reports/railtie.rb
@@ -5,6 +5,14 @@ module Reports
 
   class Railtie < Rails::Railtie
 
+    initializer "reports.load", :after => "action_dispatch.configure" do |app|
+      if ActionDispatch::Reloader.respond_to?(:to_prepare)
+        ActionDispatch::Reloader.to_prepare { Reports.reload }
+      else
+        ActionDispatch::Reloader.before { Reports.reload }
+      end
+    end
+
   end
 
 end
