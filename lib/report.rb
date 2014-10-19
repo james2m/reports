2cbf1b2ae52a60d812f9eb9f54cd3943984a2898
James McCarthy <james@thisishatch.co.uk>
2014-10-19 11:23:10 +0700

    Stop Report from using type from Reports::Base.
    Signed-off-by: James McCarthy <james@thisishatch.co.uk>

--- lib/report.rb
+++ lib/report.rb
@@ -1,17 +1,18 @@
 class Report
 
   include ActiveModel::Model
+  attr_accessor :type
 
-  delegate :type, :title, :from, :to, :period, :month, :file_name, :stream, to: :report, allow_nil: true
+  delegate :title, :from, :to, :period, :month, :file_name, :stream, to: :report, allow_nil: true
 
   def self.all
     Reports.all.map { |klass| new('type' => String(klass)) }
   end
 
   def initialize(options = {})
-    type = String(options.delete('type'))
+    @type = String(options.delete('type'))
 
-    klass = Reports.all.find { |mod| String(mod).demodulize.underscore.gsub(/_report$/, '') == type }
+    klass = Reports.all.find { |mod| String(mod) == @type }
 
     @report = klass.new(options) if klass
   end
